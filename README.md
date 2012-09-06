14856719cb26c1c4fd001807bf37e5c791f8a603
Chris Buttery <chrisbuttery@Chriss-iMac.local>
2012-09-07 08:54:30 +1000

    updated structure + added config

---
+++ README.md
@@ -0,0 +1,8 @@
++scss-dir
++========
++
++A structured SCSS Dir
++
++Requires the Stitch Framework : http://stitchcss.com/ https://github.com/anthonyshort/stitch-css
+ 	  	
+\ No newline at end of file
---
+++ config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+require "stitch"
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "src"
+images_dir = "css/assets"
+javascripts_dir = "js"
+line_comments   = false
+relative_assets = true
+output_style    = :expanded
+
+if environment == :production
+  
+elsif environment == :development
+
+end
